9d4ed28b8a5c826fb7df44e0a1e81d577e7c9e4f
Noah Gary <goahnary@gmail.com>
2016-04-21 23:00:26 -0500

    fixes

--- html/websocket.php
+++ html/websocket.php
@@ -10,11 +10,11 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
-    <script>
-	#img-here img {
-		width: 100%;
-	}
-    </script>
+    <style>
+      #imghere img {
+		    width: 100%;
+      }
+    </style>
     
   </head>
   <body>
